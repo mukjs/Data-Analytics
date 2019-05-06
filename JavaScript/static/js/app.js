1c9e2dacf29425dd9fa7de8acf3576fd9fd666f0
mukjs <46197650+mukjs@users.noreply.github.com>
2019-05-05 20:55:47 -0400

    Changing date filter to dropdown instead of input, add code to display table with all data on page load

--- JavaScript/static/js/app.js
+++ JavaScript/static/js/app.js
@@ -1,37 +1,71 @@
 // from data.js
 var tableData = data;
 
+//UFOTable 
+var table_ufo = document. getElementById("ufo-table");
+
+
+window.onload = function () {
+
+  var unique_date=[... new Set(tableData.map(sighting=> sighting.datetime))];
+  var select_date=document.getElementById("datetime");
+
+  console.log(select_date);
+
+  unique_date.forEach((date) => {
+    var option=document.createElement("option");
+    option.text=date;
+    select_date.append(option);
+  })
+  
+  var date = d3.select("#datetime");
+
+  
+
+  var tbody = d3.select("tbody");
+  tableData.forEach((sighting) => {
+      var row = tbody.append("tr");
+      Object.entries(sighting).forEach(([key, value]) => {
+        var cell = row.append("td");
+        cell.text(value);
+        });
+    });
+}
+
 // Select the submit button
 
 var submit = d3.select("#filter-btn");
 
 submit.on("click", function() {
 
-    // Prevent the page from refreshing
-    d3.event.preventDefault();
-  
-    // Select the input element and get the raw HTML node
-    var inputElement = d3.select("#datetime");
-  
-    // Get the value property of the input element
-    var inputValue = inputElement.property("value");
-  
-    console.log(inputValue);
-    console.log(tableData);
-  
-    var filteredData = tableData.filter(sighting => sighting.datetime === inputValue);
-  
-    console.log(filteredData);
+  // Prevent the page from refreshing
+  d3.event.preventDefault();
 
-    var tbody = d3.select("tbody");
+  //Clear existing data from table
+  for(var i = table_ufo.rows.length - 1; i > 0; i--){
+    table_ufo. deleteRow(i);
+  }
+  
+
+  var date = d3.select("#datetime");
+
+  // Get the value property of the input element
+  var inputValue = date.property("value");
+  
+  var filteredData = tableData.filter(sighting => sighting.datetime === inputValue);
+  
+  console.log(filteredData);
+
+  var tbody = d3.select("tbody");
 
     // // Finally, display the filtered data in html
-    filteredData.forEach((sighting) => {
-        var row = tbody.append("tr");
-        Object.entries(sighting).forEach(([key, value]) => {
-          var cell = row.append("td");
-          cell.text(value);
-        });
+  filteredData.forEach((sighting) => {
+    var row = tbody.append("tr");
+    Object.entries(sighting).forEach(([key, value]) => {
+      var cell = row.append("td");
+      cell.text(value);
       });
   });
+
+});
   
